8ae1a07b86df0f7c7f8675e3f6eccf849abc0e96
VitaliiHryshchenko <v.grischenko80@gmail.com>
2024-02-26 22:10:18 +0100

    task-5 completed

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector('.change-color');
+
+const body = document.querySelector('body');
+const colorSpan = document.querySelector('.color');
+button.addEventListener('click', handleClick);
+function handleClick(event) {
+  body.style = `background-color: ${getRandomHexColor()}`;
+  colorSpan.textContent = getRandomHexColor();
+}
