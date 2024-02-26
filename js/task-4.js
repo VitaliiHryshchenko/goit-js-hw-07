118ee8b34fcafe1c8f6ca95fda59c993a8017980
VitaliiHryshchenko <v.grischenko80@gmail.com>
2024-02-26 21:46:45 +0100

    task-4 completed

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+const button = document.querySelector('button');
+button.textContent = 'Log in';
 
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault(); // відміняє поведінку браузера за замовчуванням (відміняємо перезавантаження сторінки)
+  const elements = event.currentTarget.elements;
+  const info = {
+    email: elements.email.value.trim(),
+    password: elements.password.value.trim(),
+  };
+  if (
+    elements.email.value.trim() === '' ||
+    elements.password.value.trim() === ''
+  ) {
+    alert('All form fields must be filled in');
+  } else {
+    console.log(info);
+  }
+  event.currentTarget.reset();
+}
